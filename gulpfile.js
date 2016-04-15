155631e6ba01d6122b6682c7a039441635adee15
Owen Buckley <owen@analogstudios.net>
2016-04-15 08:07:19 -0400

    initial commit (#6)

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,30 @@
 var gulp = require('gulp');
 var taskLister = require('gulp-task-listing');
 var validatePipeline = require('pipeline-validate-js');
+var webserver = require('gulp-webserver');
+
+var serverOptions = {
+  livereload: true,
+  open: '/',
+  // proxies: [{
+  //   source: '/api',
+  //   target: 'http://analogstudios.thegreenhouse.io/api'
+  // }],
+  root: './'
+};
 
 gulp.task('lint', function () {
   gulp.src([
     './gulpfile.js'
   ]).pipe(validatePipeline.validateJS());
+});
+
+
+gulp.task('serve', function () {
+
+  return gulp.src(serverOptions.root)
+    .pipe(webserver(serverOptions));
+
 });
 
 gulp.task('help', taskLister);
