a810005477da592d92a43329ab7c59122b930d98
Owen Buckley <owen@analogstudios.net>
2016-04-15 23:42:36 -0400

    linting error

--- src/components/owner-details/owner-details.js
+++ src/components/owner-details/owner-details.js
@@ -16,7 +16,7 @@ var OwnerDetails = React.createClass({
       url: 'https://api.github.com/user',
       headers: {
         'Accept': 'application/vnd.github.v3+json',
-        'Authorization': 'token XXX'
+        'Authorization': 'token 83f15d2eb6e6029dd9d20a79551c6f701a046c95'
       }
     }).done(response => {
       this.setState({
@@ -30,7 +30,7 @@ var OwnerDetails = React.createClass({
     return (
       <section>
         <img class="owner-avatar" src={this.state.avatar}/>
-        <h1><span class="owner-name">{this.state.name}</span>'s Repositories</h1>
+        <h1><span class="owner-name">{this.state.name}</span>\'s Repositories</h1>
       </section>
     )
   }
