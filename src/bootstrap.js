d0b2df1a6a633c6e58391ff3a4b40bc36e0f4086
Owen Buckley <owen@analogstudios.net>
2016-04-16 18:51:05 -0400

    refactored app bootstrapping

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,7 +1,6 @@
 'use strict';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { render } from 'react-dom';
 import { IndexRoute, Link, Router, Route, browserHistory } from 'react-router';
 
@@ -15,12 +14,6 @@ import Navigation from './components/navigation/navigation';
 import OwnerDetails from './components/owner-details/owner-details';
 import RepositoryList from './components/repository-list/repository-list';
 
-const NotFound = React.createClass({
-  render() {
-    return <h2>Not found</h2>
-  }
-});
-
 const Bootstrap = React.createClass({
   render: function() {
     return (
@@ -28,7 +21,7 @@ const Bootstrap = React.createClass({
         <section className="row">
 
           <div className="col-md-*">
-            <section id="header"></section>
+            <Header></Header>
           </div>
 
         </section>
@@ -36,12 +29,8 @@ const Bootstrap = React.createClass({
         <section className="row">
 
           <div className="col-md-3">
-            <section id="owner-details"></section>
-            <ul>
-              <li><Link key="1" to="/personal">Personal</Link></li>
-              <li><Link key="2" to="/following">Following</Link></li>
-            </ul>
-            // <section id="navigation"></section>
+            <OwnerDetails></OwnerDetails>
+            <Navigation></Navigation>
           </div>
 
           <div className="col-md-9">
@@ -53,9 +42,10 @@ const Bootstrap = React.createClass({
         <section className="row">
 
           <div className="col-md-12">
-            <section id="footer"></section>
+            <Footer></Footer>
           </div>
         </section>
+
       </section>
     )
   }
@@ -67,27 +57,6 @@ render((
       <IndexRoute component={RepositoryList} />
       <Route path="personal" component={RepositoryList} />
       <Route path="following" component={Following} />
-      <Route path="*" component={NotFound} />
     </Route>
   </Router>
 ), document.getElementById('content'));
-
-ReactDOM.render(
-  <Header />,
-  document.getElementById('header')
-);
-
-// ReactDOM.render(
-//   <Navigation />,
-//   document.getElementById('navigation')
-// );
-
-ReactDOM.render(
-  <OwnerDetails />,
-  document.getElementById('owner-details')
-);
-
-ReactDOM.render(
-  <Footer />,
-  document.getElementById('footer')
-);
