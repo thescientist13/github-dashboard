899bdaca4ab5141d34ec2fefa0797691c3fb9de2
Owen Buckley <owen@analogstudios.net>
2016-04-15 23:42:59 -0400

    clear keys

--- src/components/repository-list/repository-list.js
+++ src/components/repository-list/repository-list.js
@@ -15,7 +15,7 @@ var RepositoryList = React.createClass({
       url: 'https://api.github.com/users/thescientist13/repos',
       headers: {
         'Accept': 'application/vnd.github.v3+json',
-        'Authorization': 'token 83f15d2eb6e6029dd9d20a79551c6f701a046c95'
+        'Authorization': 'token XXX'
       }
     }).done(response => {
       this.setState({
