10bbcac7074b84870f8674b3ff406c30b8086ef0
Owen Buckley <owen@analogstudios.net>
2016-05-10 21:33:07 -0400

    call build task in build script

--- bin/build.sh
+++ bin/build.sh
@@ -4,7 +4,16 @@ echo "node version"
 node -v
 echo "npm version"
 npm -v
-#echo "gulp version"
-#gulp -v
-#echo "jspm version"
-#jspm -v
+echo "gulp version"
+gulp -v
+echo "jspm version"
+jspm -v
+
+echo "install dependencies..."
+rm -rf node_modules/ > /dev/null 2>&1
+rm -rf jspm_packages/ > /dev/null 2>&1
+
+npm install
+jspm install
+
+gulp build
