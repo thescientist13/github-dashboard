198da1bb58cf470168ad92f85afc3efbe7f7777d
Owen Buckley <owen@analogstudios.net>
2016-06-12 15:59:39 -0400

    setup credentials

--- bin/build.sh
+++ bin/build.sh
@@ -11,6 +11,10 @@ rm -rf jspm_packages/ > /dev/null 2>&1
 
 npm install
 
+//build prep
 ./node_modules/.bin/typings install
 ./node_modules/.bin/jspm install
+cp src/credentials.ts.tmpl src/credentials.ts
+
+//build
 ./node_modules/.bin/gulp  build
