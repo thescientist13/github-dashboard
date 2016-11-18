d74510c7acb6df9f1b3ee786037009a6f1fbbeb7
Owen Buckley <owen@analogstudios.net>
2016-11-17 23:37:51 -0500

    update build script

--- bin/build.sh
+++ bin/build.sh
@@ -7,14 +7,11 @@ npm -v
 
 echo "install dependencies..."
 rm -rf node_modules/ > /dev/null 2>&1
-rm -rf jspm_packages/ > /dev/null 2>&1
 
 npm install
 
 //build prep
-npm run install:jspm
-npm run install:typings
 cp src/credentials.ts.tmpl src/credentials.ts
 
 //build
-npm run build
+npm run ci
