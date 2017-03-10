ccb256c9f146b9cf28c8a1b8e82763a94b977313
Owen Buckley <owen.buckley@analogstudios.net>
2017-03-10 08:34:08 -0500

    basic table repo test

--- src/components/repositories-table/repositories-table.spec.js
+++ src/components/repositories-table/repositories-table.spec.js
@@ -5,10 +5,38 @@ import RepositoriesTable from './repositories-table';
 
 describe('RepositoriesTable Component', () => {
 
-  xit('renders without crashing', () => {
+  it('renders without crashing', () => {
     const div = document.createElement('div');
 
-    render(<TableRepositories/>, div);
+    render(<RepositoriesTable
+      repositories={[]}
+      hasMoreRepos={false}
+      nextReposUrl={undefined}/>, div);
   });
 
+  it('should render a table with one repo', () => {
+    const div = document.createElement('div');
+
+    let repos = [{
+      details: {
+        name: 'test-repo',
+        hasAssignedIssues: true,
+        html_url: 'http://api.github.com/my-org/my-repo'
+      },
+      issues: 3,
+      pullRequests: 1,
+      openIssues: 2
+    }];
+
+    const table = shallow(<RepositoriesTable repositories={repos} hasMoreRepos={true} nextReposUrl={'http://api.github.com/?page=2'}/>);
+
+    expect(table.find('table').length).toEqual(1);
+    expect(table.find('thead').length).toEqual(1);
+    expect(table.find('tbody').length).toEqual(1);
+    expect(table.find('tr').length).toEqual(2);
+    expect(table.find('th').length).toEqual(4);
+    expect(table.find('td').length).toEqual(4);
+  });
+
+  //TODO good oppourtunity for snapshot testing?
 });
