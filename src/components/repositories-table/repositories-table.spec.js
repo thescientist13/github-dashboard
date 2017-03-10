e72db200519d6edeb9cbef71f2eca71186580369
Owen Buckley <owen.buckley@analogstudios.net>
2017-03-10 08:37:01 -0500

    basic table repo test

--- src/components/repositories-table/repositories-table.spec.js
+++ src/components/repositories-table/repositories-table.spec.js
@@ -28,7 +28,7 @@ describe('RepositoriesTable Component', () => {
       openIssues: 2
     }];
 
-    const table = shallow(<RepositoriesTable repositories={repos} hasMoreRepos={true} nextReposUrl={'http://api.github.com/?page=2'}/>);
+    const table = shallow(<RepositoriesTable repositories={repos} hasMoreRepos={false} nextReposUrl={undefined}/>);
 
     expect(table.find('table').length).toEqual(1);
     expect(table.find('thead').length).toEqual(1);
@@ -36,6 +36,26 @@ describe('RepositoriesTable Component', () => {
     expect(table.find('tr').length).toEqual(2);
     expect(table.find('th').length).toEqual(4);
     expect(table.find('td').length).toEqual(4);
+    expect(table.find('button').length).toEqual(0);
+  });
+
+  it('should render a load more repos button', () => {
+    const div = document.createElement('div');
+
+    let repos = [{
+      details: {
+        name: 'test-repo',
+        hasAssignedIssues: true,
+        html_url: 'http://api.github.com/my-org/my-repo'
+      },
+      issues: 3,
+      pullRequests: 1,
+      openIssues: 2
+    }];
+
+    const table = shallow(<RepositoriesTable repositories={repos} hasMoreRepos={true} nextReposUrl={'http://api.github.com?page=2'}/>);
+
+    expect(table.find('button').length).toEqual(1);
   });
 
   //TODO good oppourtunity for snapshot testing?
