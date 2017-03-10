ff1ec35285818ab469166491b4bc8ab1217553bc
Owen Buckley <owen.buckley@analogstudios.net>
2017-03-10 08:20:33 -0500

    store tests

--- src/stores/github-store.spec.js
+++ src/stores/github-store.spec.js
@@ -5,6 +5,14 @@ import MOCK_USER_DETAILS from '../../test/mocks/user-details.json';
 import MOCK_USER_REPOS from '../../test/mocks/user-repositories.json';
 import MOCK_USER_SUBSCRIPTIONS from '../../test/mocks/user-subscriptions.json';
 
+const MOCK_INIT_STATE = {
+  userDetails: {},
+  userRepositories: [],
+  userSubscriptions: [],
+  hasMoreRepos: false,
+  nextReposUrl: ''
+};
+
 describe('GitHub Store Actions', () => {
 
   it('should get user details', () => {
@@ -75,13 +83,125 @@ describe('GitHub Store Reducer', () => {
   it('should return the initial state', () => {
     expect(
       githubStoreReducer(undefined, {})
+    ).toEqual(MOCK_INIT_STATE)
+  });
+
+  it('should return user details', () => {
+    expect(
+      githubStoreReducer(MOCK_INIT_STATE, {
+        type: GITHUB_STORE_ACTIONS.GET_USER_DETAILS,
+        userDetails: {
+          username: MOCK_USER_DETAILS.login,
+          avatar: MOCK_USER_DETAILS.avatar_url
+        }
+      })
     ).toEqual({
-      userDetails: {},
+      userDetails: {
+        username: MOCK_USER_DETAILS.login,
+        avatar: MOCK_USER_DETAILS.avatar_url
+      },
       userRepositories: [],
       userSubscriptions: [],
       hasMoreRepos: false,
       nextReposUrl: ''
     })
-  })
+  });
+
+  it('should return user repositories', () => {
+    let slicedRepo = MOCK_USER_REPOS.slice(0, 1);
+
+    expect(
+      githubStoreReducer(MOCK_INIT_STATE, {
+        type: GITHUB_STORE_ACTIONS.GET_USER_REPOSITORIES,
+        userRepositories: [{
+          id: slicedRepo.id,
+          details: slicedRepo,
+        }],
+        hasMoreRepos: false,
+      })
+    ).toEqual({
+      userDetails: {},
+      userRepositories: [{
+        "details": slicedRepo,
+        "id": slicedRepo.id,
+        "issues": {
+          "count": 0,
+          "hasAssignedIssues": false,
+          "issues": [],
+          "openIssues": 0,
+          "pullRequests": 0,
+        }
+      }],
+      userSubscriptions: [],
+      hasMoreRepos: false,
+      nextReposUrl: undefined
+    })
+  });
+
+  //TODO test with more repos
+
+  it('should return get user subscriptions', () => {
+    let slicedRepo = MOCK_USER_SUBSCRIPTIONS.slice(0, 1);
+
+    expect(
+      githubStoreReducer(MOCK_INIT_STATE, {
+        type: GITHUB_STORE_ACTIONS.GET_USER_SUBSCRIPTIONS,
+        userSubscriptions: [{
+          id: slicedRepo.id,
+          details: slicedRepo,
+        }],
+        hasMoreRepos: false,
+      })
+    ).toEqual({
+      userDetails: {},
+      userSubscriptions: [{
+        "details": slicedRepo,
+        "id": slicedRepo.id,
+        "issues": {
+          "count": 0,
+          "hasAssignedIssues": false,
+          "issues": [],
+          "openIssues": 0,
+          "pullRequests": 0,
+        }
+      }],
+      userRepositories: [],
+      hasMoreRepos: false,
+      nextReposUrl: undefined
+    })
+  });
+
+  xit('should return get user subscriptions on top of user details', () => {
+    expect(
+      githubStoreReducer(MOCK_INIT_STATE, {
+        type: GITHUB_STORE_ACTIONS.GET_USER_DETAILS,
+        userDetails: {
+          username: MOCK_USER_DETAILS.login,
+          avatar: MOCK_USER_DETAILS.avatar_url
+        }
+      })
+    ).toEqual({
+      userDetails: {
+        username: MOCK_USER_DETAILS.login,
+        avatar: MOCK_USER_DETAILS.avatar_url
+      },
+      userRepositories: [],
+      userSubscriptions: [],
+      hasMoreRepos: false,
+      nextReposUrl: ''
+    })
+  });
+
+
+  xit('should return get user repositories and initial user details', () => {
+  });
+
+
+  xit('should return get user subscriptions and initial user details', () => {
+  });
+
+
+  xit('should test more state permutations, eg subs, repos and user details', () => {
+  });
 
 });
