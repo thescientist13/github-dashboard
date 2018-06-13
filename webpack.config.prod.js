5c2ea45bf60362cc05299046cb7bed6b01fccfc4
Owen Buckley <owen.buckley@clarivate.com>
2018-06-13 16:20:57 -0400

    build task working

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,54 +1,23 @@
 const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.config.common');
-const ProvidePlugin = require('webpack/lib/ProvidePlugin');
-const DefinePlugin = require('webpack/lib/DefinePlugin');
-const NormalModuleReplacementPlugin = require('webpack/lib/NormalModuleReplacementPlugin');
-const IgnorePlugin = require('webpack/lib/IgnorePlugin');
-const DedupePlugin = require('webpack/lib/optimize/DedupePlugin');
-const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
-const WebpackMd5Hash = require('webpack-md5-hash');
-
-const ENVIRONMENT = process.env.NODE_ENV = process.env.ENV = 'production';
-
 
 module.exports = webpackMerge(commonConfig, {
 
-  debug: false,
+  mode: 'production',
 
-  devtool: 'source-map',
-
-  output: {
-    path: __dirname + '/build',
-    filename: '[name].[chunkhash].bundle.js',
-    sourceMapFilename: '[name].[chunkhash].bundle.map',
-    chunkFilename: '[id].[chunkhash].chunk.js'
-  },
-
-  plugins: [
-
-    new WebpackMd5Hash(),
-    new DedupePlugin(),
-
-    new DefinePlugin({
-      'ENV': JSON.stringify(ENVIRONMENT),
-      'process.env': {
-        'ENV': JSON.stringify(ENVIRONMENT),
-        'NODE_ENV': JSON.stringify(ENVIRONMENT)
-      }
-    }),
-
-    new UglifyJsPlugin({
-      beautify: false,
-      mangle: { screw_ie8 : true, keep_fnames: true },
-      compress: { screw_ie8: true },
-      comments: false
-    })
-  ],
-
-  tslint: {
-    emitErrors: true,
-    failOnHint: true,
-    resourcePath: 'src'
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      enforce: 'pre',
+      use: [{
+        loader: 'tslint-loader',
+        options: {
+          emitErrors: true,
+          failOnHint: true,
+          resourcePath: 'src'
+        }
+      }]
+    }]
   }
 
-});
+})
